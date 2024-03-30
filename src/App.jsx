93a81ad02e9d40ae0e4df96a93665e46c54db502
Hasan Ashrafi <yasashrafi@gmail.com>
2024-03-30 11:27:39 +0330

    added layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
 import HomePage from './components/templates/HomePage'
+import Layout from './layouts/layout'
 
-// const BASE_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en&x_cg_demo_api_key=CG-JbJLb9BfQY1ZGzBHXgjzsnuk"
 
 
 function App() {
-  // const coins = () => {
-  //   fetch(BASE_URL)
-  //     .then(res => res.json())
-  //     .then(json => console.log(json))
-  // }
-  // coins()
+ 
   return (
-  
-    
-      <HomePage/> 
-     
+    <Layout>
+    <HomePage />
+    </Layout>
+
+
+
+
+
   )
 }
 
